de342466f144c5654c289ae82d5c576a1b6ed3d3
Evan <evanejk@gmail.com>
2024-02-03 14:34:12 -0500

    keep track of every 3rd day too for cleaning the humidifier

--- app/src/main/java/com/happynicetime/everyotherday/MainActivity.kt
+++ app/src/main/java/com/happynicetime/everyotherday/MainActivity.kt
@@ -7,25 +7,41 @@ import java.util.Calendar
 import java.util.TimeZone
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textViewDay: TextView
+    private lateinit var textViewDays2: TextView
+    private lateinit var textViewDays3: TextView
+    //private lateinit var textViewDays4: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        textViewDay = findViewById<TextView>(R.id.textViewDay)
+        textViewDays2 = findViewById<TextView>(R.id.textViewDays2)
+        textViewDays3 = findViewById<TextView>(R.id.textViewDays3)
+        //textViewDays4 = findViewById<TextView>(R.id.textViewDays4)
         updateDay()
     }
     override fun onResume() {
         super.onResume()
         updateDay()
     }
-    private fun updateDay(){
+    private fun updateDay() {
         val c = Calendar.getInstance()
-        var timeInt = ((TimeZone.getDefault().rawOffset + c.getTimeInMillis()) / (1000 * 60 * 60 * 24)).toInt()
+        var timeInt =
+            ((TimeZone.getDefault().rawOffset + c.getTimeInMillis()) / (1000 * 60 * 60 * 24)).toInt()
         var isOddDay = timeInt % 2
-        if(isOddDay == 1){
-            textViewDay.setText("\"A\"\nday")
-        }else{
-            textViewDay.setText("\"B\"\nday")
+        if (isOddDay == 1) {
+            textViewDays2.setText("\"2A\"")
+        } else {
+            textViewDays2.setText("\"2B\"")
         }
+        when(timeInt % 3){
+            1 -> textViewDays3.setText("\"3A\"")
+            2 -> textViewDays3.setText("\"3B\"")
+            0 -> textViewDays3.setText("\"3C\"")
+        }
+        /*when(timeInt % 4){
+            1 -> textViewDays4.setText("\"4A\"")
+            2 -> textViewDays4.setText("\"4B\"")
+            3 -> textViewDays4.setText("\"4C\"")
+            0 -> textViewDays4.setText("\"4D\"")
+        }*/
     }
 }
